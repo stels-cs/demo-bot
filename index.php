7d698047a20483d13fc721930d55f8d1a525dbae
Tom Nolane <gotopredestenacia@gmail.com>
2018-07-02 15:26:37 +0300

    change $userId = $message['from_id']

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
-
 require_once './vendor/autoload.php';
 
-
 const VK_TOKEN = 'ae51ff9f59f0c40f029c67c3588466bcec1f7ff0f333404769952a31290c3cf097f5b7c0cfd90b0da4562';
-
-use VK\Client\Enums\VKLanguage;
-use VK\Client\VKApiClient;
 
 function myLog($str) {
     file_put_contents("php://stdout", "$str\n");
@@ -16,7 +11,6 @@ const COLOR_NEGATIVE = 'negative';
 const COLOR_POSITIVE = 'positive';
 const COLOR_DEFAULT = 'default';
 const COLOR_PRIMARY = 'primary';
-
 const CMD_ID = 'ID';
 const CMD_NEXT = 'NEXT';
 const CMD_TYPING = 'TYPING';
@@ -31,48 +25,32 @@ function getBtn($label, $color, $payload = '') {
         'color' => $color
     ];
 }
-
-
 $json = file_get_contents('php://input');
 //myLog($json);
-
-
 $data = json_decode($json, true);
-
 $type = $data['type'] ?? '';
-
 $vk = new VKApiClient('5.78', VKLanguage::RUSSIAN);
-
 if ($type === 'message_new') {
-
-
     $message = $data['object'] ?? [];
-    $userId = $message['user_id'] ?? 0;
+    $userId = $message['from_id'] ?? 0; //this need to change
     $body = $message['body'] ?? '';
-
     $payload = $message['payload'] ?? '';
-
     if ($payload) {
         $payload = json_decode($payload, true);
     }
-
-    myLog("MSG: ".$body." PAYLOAD:".$payload);
-
+    //myLog($userId);
+   myLog("MSG: ".$body." PAYLOAD:".$payload);
     $kbd = [
         'one_time' => false,
         'buttons' => [
             [getBtn("Покажи мой ID", COLOR_DEFAULT, CMD_ID)],
             [getBtn("Далее", COLOR_PRIMARY, CMD_NEXT)],
-
         ]
     ];
-
     $msg = "Привет я бот!";
-
     if ($payload === CMD_ID) {
         $msg = "Ваш id ".$userId;
     }
-
     if ($payload === CMD_NEXT) {
         $kbd = [
             'one_time' => false,
@@ -82,7 +60,6 @@ if ($type === 'message_new') {
             ]
         ];
     }
-
     if ($payload === CMD_TYPING) {
         try {
             $res = $vk->messages()->setActivity(VK_TOKEN, [
@@ -94,7 +71,6 @@ if ($type === 'message_new') {
             myLog( $e->getCode().' '.$e->getMessage() );
         }
     }
-
     try {
         if ($msg !== null) {
             $response = $vk->messages()->send(VK_TOKEN, [
@@ -106,8 +82,5 @@ if ($type === 'message_new') {
     } catch (\Exception $e) {
         myLog( $e->getCode().' '.$e->getMessage() );
     }
-
 }
-
-
 echo  "OK";
